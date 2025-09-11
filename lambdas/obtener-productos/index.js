ad9fc248dd51340e78fdb0e1599e75995a89a972
DavidVillavicencio <dvillavicencio@bolivariano.com>
2025-09-11 01:39:12 -0500

    Se agrega al YAML (CI/CD) el despliegue de los metodos al API Gateway.

--- lambdas/obtener-productos/index.js
+++ lambdas/obtener-productos/index.js
@@ -1,8 +1,11 @@
 exports.handler = async (event) => {
     const productos = [
-        { id: 1, nombre: "Arroz 5kg", precio: 10.5 },
-        { id: 2, nombre: "Aceite 1L", precio: 3.2 },
-        { id: 3, nombre: "Papel Higiénico (12 rollos)", precio: 7.5 }
+        { id: 1, nombre: "Camiseta Nike", precio: 60.50 },
+        { id: 2, nombre: "Camisa Manga Larga EL", precio: 35.20 },
+        { id: 3, nombre: "Blue Jeans Tomy Hilfiger", precio: 70.50 },
+        { id: 4, nombre: "Pantalon de Tela INDIE", precio: 55.50 },
+        { id: 5, nombre: "Abrigo Puma para Hombre", precio: 75.99 },
+        { id: 6, nombre: "Abrigo Adidas para Mujer", precio: 88.00 },
     ];
 
     return {

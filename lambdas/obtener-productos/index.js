22147c15fd21dbd64c2952244bc752704baa4dfe
DavidVillavicencio <dvillavicencio@bolivariano.com>
2025-09-14 21:27:28 -0500

    Se agregan imagenes en la salida de obtener productos

--- lambdas/obtener-productos/index.js
+++ lambdas/obtener-productos/index.js
@@ -1,11 +1,11 @@
 exports.handler = async (event) => {
     const productos = [
-        { id: 1, nombre: "Camiseta Nike", precio: 60.50 },
-        { id: 2, nombre: "Camisa Manga Larga EL", precio: 35.20 },
-        { id: 3, nombre: "Blue Jeans Tomy Hilfiger", precio: 70.50 },
-        { id: 4, nombre: "Pantalon de Tela INDIE", precio: 55.50 },
-        { id: 5, nombre: "Abrigo Puma para Hombre", precio: 75.99 },
-        { id: 6, nombre: "Abrigo Adidas para Mujer", precio: 88.00 },
+        { id: 1, nombre: "Camiseta Nike", precio: 60.50, imagen: "/wp-content/uploads/2025/09/camiseta1.png" },
+        { id: 2, nombre: "Camisa Manga Larga EL", precio: 35.20, imagen: "/wp-content/uploads/2025/09/camiseta2.png" },
+        { id: 3, nombre: "Blue Jeans Tomy Hilfiger", precio: 70.50, imagen: "/wp-content/uploads/2025/09/bluejean.png" },
+        { id: 4, nombre: "Pantalon de Tela INDIE", precio: 55.50, imagen: "/wp-content/uploads/2025/09/tela.png" },
+        { id: 5, nombre: "Abrigo Puma para Hombre", precio: 75.99, imagen: "/wp-content/uploads/2025/09/abrigo1.png" },
+        { id: 6, nombre: "Abrigo Adidas para Mujer", precio: 88.00, imagen: "/wp-content/uploads/2025/09/abrigo2.png" },
     ];
 
     return {
